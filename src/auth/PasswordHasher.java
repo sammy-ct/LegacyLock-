5e60c72a1397b1e842fb40604bf1be7d2293692c
Soumi Samanta <soumiiem31@gmail.com>
2026-03-17 22:18:32 +0530

    Added password hashing security module

---
+++ src/auth/PasswordHasher.java
@@ -0,0 +1,27 @@
+package auth;
+
+import java.security.MessageDigest;
+
+public class PasswordHasher {
+
+    public static String hash(String password){
+
+        try{
+
+            MessageDigest md=MessageDigest.getInstance("SHA-256");
+
+            byte[] hash=md.digest(password.getBytes());
+
+            StringBuilder sb=new StringBuilder();
+
+            for(byte b:hash){
+                sb.append(String.format("%02x",b));
+            }
+
+            return sb.toString();
+
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
